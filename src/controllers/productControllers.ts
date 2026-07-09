f1be173bf7f49553cb6f99d47f303e3cc489c1a0
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2026-07-09 16:30:34 +0700

    productControllers

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -3,3 +3,13 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma";
 
 const prisma = new PrismaClient();
+
+export const getProducts = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving products" });
+  }
+};
