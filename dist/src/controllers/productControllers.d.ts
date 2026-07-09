31cb51634b66e927578f90824ff02b11537a8989
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2026-07-09 11:03:10 +0700

    update

---
+++ dist/src/controllers/productControllers.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=productControllers.d.ts.map
---
+++ src/controllers/productControllers.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
