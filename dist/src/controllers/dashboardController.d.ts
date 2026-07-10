5f47a5c40419b6bea8d99d2334b312b76b3eb439
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2026-07-10 09:35:13 +0700

    Delete dist directory

--- dist/src/controllers/dashboardController.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-export declare const getDashboardMetrics: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=dashboardController.d.ts.map
--- dist/src/controllers/productControllers.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from "express";
-export declare const getProducts: (req: Request, res: Response) => Promise<void>;
-export declare const createProduct: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=productControllers.d.ts.map
