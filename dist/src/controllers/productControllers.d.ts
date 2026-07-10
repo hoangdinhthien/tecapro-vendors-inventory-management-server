945bb5f4a0e6518aae5d854fd654e9907ae8a65c
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2026-07-10 09:25:42 +0700

    update

--- dist/src/controllers/productControllers.d.ts
+++ dist/src/controllers/productControllers.d.ts
@@ -1,2 +1,4 @@
-export {};
+import { Request, Response } from "express";
+export declare const getProducts: (req: Request, res: Response) => Promise<void>;
+export declare const createProduct: (req: Request, res: Response) => Promise<void>;
 //# sourceMappingURL=productControllers.d.ts.map
--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma";
 
 const prisma = new PrismaClient();
@@ -9,7 +8,37 @@ export const getProducts = async (
   res: Response,
 ): Promise<void> => {
   try {
+    const search = req.query.search?.toString();
+    const products = await prisma.products.findMany({
+      where: {
+        name: {
+          contains: search,
+        },
+      },
+    });
+    res.json(products);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving products" });
+  }
+};
+
+export const createProduct = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { productId, name, price, rating, stockQuantity } = req.body;
+    const product = await prisma.products.create({
+      data: {
+        productId,
+        name,
+        price,
+        rating,
+        stockQuantity,
+      },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating product" });
   }
 };
